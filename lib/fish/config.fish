e541370c457322d74492d6e58da69e6601aaba58
Yuta Fujita <ofnhwx@komunan.net>
2020-02-19 10:13:31 +0900

    update: いくつかの機能を dotfiles の管理下に移動

--- lib/fish/config.fish
+++ lib/fish/config.fish
@@ -1,4 +1,4 @@
-# -*- mode: conf-space; coding: utf-8 -*-
+# -*- mode: fish; coding: utf-8 -*-
 # Last updated: <2017/12/01 17:11:28>
 
 # Locale
@@ -14,38 +14,36 @@ set -x LC_TIME ja_JP.UTF-8
 set -x XAPIAN_CJK_NGRAM 1
 
 # anyenv
-set PATH "$HOME/.anyenv/bin" $PATH
-status --is-interactive; and source (anyenv init -|psub)
-# https://github.com/rbenv/ruby-build/issues/1085#issuecomment-313604449
-switch (uname)
-case Darwin
-  set -x RUBY_CONFIGURE_OPTS --with-openssl-dir=/opt/local/
+if status --is-interactive
+    set PATH "$HOME/.anyenv/bin" $PATH
+    source (anyenv init -|psub)
+    # https://github.com/rbenv/ruby-build/issues/1085#issuecomment-313604449
+    switch (uname)
+        case Darwin
+            set -x RUBY_CONFIGURE_OPTS --with-openssl-dir=/opt/local/
+    end
 end
 
-# go
-set PATH "$GOPATH/bin" $PATH
-
-# ghq
-set -x GHQROOT (ghq root)
-set PATH "$GHQROOT/github.com/gauteh/lieer" $PATH
-set PATH "$GHQROOT/github.com/junegunn/fzf/bin" $PATH
+# others
+test -d "$GOPATH"; and set PATH "$GOPATH/bin" $PATH
+set PATH "$HOME/bin" $PATH
 
 # keybind
 function fish_user_key_bindings
-  bind \c] '__ghq_repository_search'
-  bind \ct '__fzf_find_file'
-  bind \cr '__fzf_reverse_isearch'
-  bind \ec '__fzf_cd'
-  bind \eC '__fzf_cd --hidden'
-  bind \cg '__fzf_open'
-  bind \co '__fzf_open --editor'
+    bind \c] '__ghq_repository_search'
+    bind \ct '__fzf_find_file'
+    bind \cr '__fzf_reverse_isearch'
+    bind \ec '__fzf_cd'
+    bind \eC '__fzf_cd --hidden'
+    bind \cg '__fzf_open'
+    bind \co '__fzf_open --editor'
 end
 
 # WSL
 if uname -r | grep Microsoft > /dev/null
-  export GTK_IM_MODULE=fcitx
-  export QT_IM_MODULE=fcitx
-  export XMODIFIERS=@im=fcitx
-  export DefaultIMModule=fcitx
-  export DISPLAY=:0
+    export GTK_IM_MODULE=fcitx
+    export QT_IM_MODULE=fcitx
+    export XMODIFIERS=@im=fcitx
+    export DefaultIMModule=fcitx
+    export DISPLAY=:0
 end
