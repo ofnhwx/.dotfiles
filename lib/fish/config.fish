b77d5ba47a16287af96b2a2de0e37e126504f982
Yuta Fujita <ofnhwx@komunan.net>
2020-07-31 23:31:18 +0900

    update: fish 関連の設定を大幅に見直し

--- lib/fish/config.fish
+++ lib/fish/config.fish
@@ -1,7 +1,9 @@
 # -*- mode: fish; coding: utf-8 -*-
 # Last updated: <2017/12/01 17:11:28>
 
+# ================================================================
 # Locale
+# ================================================================
 set -x LANG ja_JP.UTF-8
 set -x LC_COLLATE ja_JP.UTF-8
 set -x LC_CTYPE ja_JP.UTF-8
@@ -10,38 +12,48 @@ set -x LC_MONETARY ja_JP.UTF-8
 set -x LC_NUMERIC ja_JP.UTF-8
 set -x LC_TIME ja_JP.UTF-8
 
-# Notmuch
-set -x XAPIAN_CJK_NGRAM 1
+# ================================================================
+# fisher
+# ================================================================
+# -- Changing the installation path
+set -g fisher_path $__fish_config_dir/fisher
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+for file in $fisher_path/conf.d/*.fish
+    builtin source $file 2> /dev/null
+end
+# -- Bootstrap installation
+if not functions -q fisher
+    curl https://git.io/fisher --create-dirs -sLo $__fish_config_dir/functions/fisher.fish
+    fish -c fisher
+end
 
+# ================================================================
 # anyenv
+# ================================================================
 if status --is-interactive
     set PATH "$HOME/.anyenv/bin" $PATH
     source (anyenv init -|psub)
-    # https://github.com/rbenv/ruby-build/issues/1085#issuecomment-313604449
-    switch (uname)
-        case Darwin
-            set -x RUBY_CONFIGURE_OPTS --with-openssl-dir=/opt/local/
-    end
 end
 
+# ================================================================
+# Go
+# ================================================================
+if which go > /dev/null
+    set GOPATH "$HOME/go"
+    set PATH "$GOPATH/bin" $PATH
+    # go get github.com/x-motemen/ghq
+end
+
+# ================================================================
 # others
-set GOPATH "$HOME/go"
-set PATH "$GOPATH/bin" $PATH
+# ================================================================
 set PATH "$HOME/bin" $PATH
 set PATH "$HOME/.local/bin" $PATH
 
-# keybind
-function fish_user_key_bindings
-    bind \c] '__ghq_repository_search'
-    bind \ct '__fzf_find_file'
-    bind \cr '__fzf_reverse_isearch'
-    bind \ec '__fzf_cd'
-    bind \eC '__fzf_cd --hidden'
-    bind \cg '__fzf_open'
-    bind \co '__fzf_open --editor'
-end
-
+# ================================================================
 # WSL
+# ================================================================
 if uname -r | grep -i microsoft > /dev/null
     export GTK_IM_MODULE=fcitx
     export QT_IM_MODULE=fcitx
